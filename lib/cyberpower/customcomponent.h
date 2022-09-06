a332c5996e42279057447a8229a1d46f57c23be3
Ryan J. Geyer <me@ryangeyer.com>
2022-09-06 13:21:20 -0700

    Minor tweaks to string parsing for status. Removed dummy UART receiver which was interfering with the component getting UART data

--- lib/cyberpower/customcomponent.h
+++ lib/cyberpower/customcomponent.h
@@ -1,113 +1,124 @@
 #include <esphome.h>
 #include <string>
 
-class CyberPowerUps : public PollingComponent, public UARTDevice {
-  public:
-    CyberPowerUps(UARTComponent *parent) : UARTDevice(parent) {}
+class CyberPowerUps : public PollingComponent, public UARTDevice
+{
+public:
+  CyberPowerUps(UARTComponent *parent) : UARTDevice(parent) {}
 
-    Sensor *inputVolts = new Sensor();
-    Sensor *outputVolts = new Sensor();
-    Sensor *loadPercent = new Sensor();
-    Sensor *temperature = new Sensor();
-    Sensor *batteryPercent = new Sensor();
-    Sensor *inputFreqHz = new Sensor();
-    Sensor *outputFreqHz = new Sensor();
-    Sensor *runtimeMinutes = new Sensor();
-    Sensor *loadHundredWatts = new Sensor();
-    // BinarySensor *upsStatus = new BinarySensor();
-    // BinarySensor *lineStatus = new BinarySensor();
-    // BinarySensor *batteryLow = new BinarySensor();
-    // BinarySensor *upsTesting = new BinarySensor();
-    // BinarySensor *upsRunning = new BinarySensor();
+  Sensor *inputVolts = new Sensor();
+  Sensor *outputVolts = new Sensor();
+  Sensor *loadPercent = new Sensor();
+  Sensor *temperature = new Sensor();
+  Sensor *batteryPercent = new Sensor();
+  Sensor *inputFreqHz = new Sensor();
+  Sensor *outputFreqHz = new Sensor();
+  Sensor *runtimeMinutes = new Sensor();
+  Sensor *loadHundredWatts = new Sensor();
+  // BinarySensor *upsStatus = new BinarySensor();
+  // BinarySensor *lineStatus = new BinarySensor();
+  // BinarySensor *batteryLow = new BinarySensor();
+  // BinarySensor *upsTesting = new BinarySensor();
+  // BinarySensor *upsRunning = new BinarySensor();
 
-    void setup() override {
-      this->set_update_interval(3000);
+  void setup() override
+  {
+    this->set_update_interval(3000);
+  }
+
+  void loop() override
+  {
+  }
+
+  void writeSensor(Sensor *s, char *val, int len)
+  {
+    if (s != nullptr && len)
+    {
+      val[len] = '\0'; // Terminate the val
+      // ESP_LOGD("cyberpower_ups", val);
+      s->publish_state((float)atof(val));
     }
+  }
 
-    void loop() override {
-    }
+  void update() override
+  {
+    write_array(new byte[2]{0x44, 0x0D}, 2); // D\r
 
-    void writeSensor(Sensor* s, char* val, int len) {
-      if (s != nullptr) {
-        val[len + 1] = '\0'; // Terminate the val
-        ESP_LOGD("cyberpower_ups", val);
-        s->publish_state((float)atof(val));
-      }
-    }
+    const int max_value_length = 16;
+    static char buffer[max_value_length];
+    int pos = 0;
+    Sensor *currentSensor = nullptr;
 
-    void update() override {
-      write_array(new byte[2] { 0x44, 0x0D }, 2); // D\r
-
-      const int max_value_length = 16;
-      static char buffer[max_value_length];
-      int pos = 0;
-      Sensor *currentSensor = nullptr;
-
-      while (available()) {
-        char rchar = read();
-        ESP_LOGD("cyberpower_ups", "Read char %c", rchar);
-        switch (rchar) {
-          case 'I':
-            writeSensor(currentSensor, buffer, pos);
-            currentSensor = this->inputVolts;
-            pos = 0;
-            // Should I nuke the rest of the buffer?
-            break;
-          case 'O':
-            writeSensor(currentSensor, buffer, pos);
-            currentSensor = this->outputVolts;
-            pos = 0;
-            break;
-          case 'L':
-            writeSensor(currentSensor, buffer, pos);
-            currentSensor = this->loadPercent;
-            pos = 0;
-            break;
-          case 'B':
-            writeSensor(currentSensor, buffer, pos);
-            currentSensor = this->batteryPercent;
-            pos = 0;
-            break;
-          case 'T':
-            writeSensor(currentSensor, buffer, pos);
-            currentSensor = this->temperature;
-            pos = 0;
-            break;
-          case 'F':
-            writeSensor(currentSensor, buffer, pos);
-            currentSensor = this->outputFreqHz;
-            pos = 0;
-            break;
-          case 'H':
-            writeSensor(currentSensor, buffer, pos);
-            currentSensor = this->inputFreqHz;
-            pos = 0;
-            break;
-          case 'R':
-            writeSensor(currentSensor, buffer, pos);
-            currentSensor = this->runtimeMinutes;
-            pos = 0;
-            break;
-          case 'Q':
-            writeSensor(currentSensor, buffer, pos);
-            currentSensor = this->loadHundredWatts;
-            pos = 0;
-            break;
-          case 'S':
-            writeSensor(currentSensor, buffer, pos);
-            currentSensor = this->outputVolts;
-            pos = 0;
-            break;
-          case '\r':
-            pos = 0;
-            break;
-          default:
-            buffer[pos++] = rchar;
-            if (pos == max_value_length-1) {
-              ESP_LOGD("cyberpower_ups", "Buffer overflow");
-              pos=0;
-            }
+    while (available())
+    {
+      char rchar = read();
+      // ESP_LOGD("cyberpower_ups", "Read char %c", rchar);
+      switch (rchar)
+      {
+      case 'I':
+        writeSensor(currentSensor, buffer, pos);
+        currentSensor = this->inputVolts;
+        pos = 0;
+        // Should I nuke the rest of the buffer?
+        break;
+      case 'O':
+        writeSensor(currentSensor, buffer, pos);
+        currentSensor = this->outputVolts;
+        pos = 0;
+        break;
+      case 'L':
+        writeSensor(currentSensor, buffer, pos);
+        currentSensor = this->loadPercent;
+        pos = 0;
+        break;
+      case 'B':
+        writeSensor(currentSensor, buffer, pos);
+        currentSensor = this->batteryPercent;
+        pos = 0;
+        break;
+      case 'T':
+        writeSensor(currentSensor, buffer, pos);
+        currentSensor = this->temperature;
+        pos = 0;
+        break;
+      case 'F':
+        writeSensor(currentSensor, buffer, pos);
+        currentSensor = this->outputFreqHz;
+        pos = 0;
+        break;
+      case 'H':
+        writeSensor(currentSensor, buffer, pos);
+        currentSensor = this->inputFreqHz;
+        pos = 0;
+        break;
+      case 'R':
+        writeSensor(currentSensor, buffer, pos);
+        currentSensor = this->runtimeMinutes;
+        pos = 0;
+        break;
+      case 'Q':
+        writeSensor(currentSensor, buffer, pos);
+        currentSensor = this->loadHundredWatts;
+        pos = 0;
+        break;
+      case '#':
+        pos = 0;
+        break;
+      case 'S':
+        writeSensor(currentSensor, buffer, pos);
+        pos = 0;
+        break;
+      case '\r':
+        pos = 0;
+        break;
+      default:
+        buffer[pos++] = rchar;
+        if (pos == max_value_length - 1)
+        {
+          ESP_LOGD("cyberpower_ups", "Buffer overflow");
+          pos = 0;
         }
       }
     }
+  }
 };
